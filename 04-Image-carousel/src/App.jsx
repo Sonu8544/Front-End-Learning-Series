18766694708c4cb24a00b336c29f4483d30acf5e
Sonu Kumar <sksonu8544@gmail.com>
2024-02-11 10:04:27 +0530

    fix coundown

--- 04-Image-carousel/src/App.jsx
+++ 04-Image-carousel/src/App.jsx
@@ -1,6 +1,5 @@
-import { useEffect } from 'react';
 import './App.css'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 function App() {
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 https://github.com/Sonu8544/Front-End-Interview-Series.git
+npm install
 npm run dev
