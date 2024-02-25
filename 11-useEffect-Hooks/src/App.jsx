95ab9e838a2994724d453d15350c9b1b33cd9ca8
Sonu Kumar <sksonu8544@gmail.com>
2024-02-25 19:59:53 +0530

    useEffect Hooks in react

--- 11-useEffect-Hooks/src/App.jsx
+++ 11-useEffect-Hooks/src/App.jsx
@@ -2,7 +2,10 @@ import './App.css'
 
 export default function App() {
   return (
-    <h1>Lorem ipsum dolor sit.</h1>
+    <>
+      <h1>Lorem ipsum dolor sit.</h1>
+      <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, assumenda laudantium illo ab fugiat debitis nesciunt repellendus accusamus doloremque facilis enim. Mollitia eum porro repudiandae perferendis labore. Perspiciatis, minus inventore! Voluptatum culpa mollitia ipsam quam modi! Esse illo assumenda laudantium.</h1>
+    </>
   )
 }
 
